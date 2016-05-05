bf130c5667f813b69c1d8ff39818cc2a9d7a23ad
ThePkms <r.sekaleshfar@gmail.com>
2016-05-05 14:21:36 +0430

    Sending files to Model folder

--- TheGame/src/Model/EnemyGroup.java
+++ TheGame/src/Model/EnemyGroup.java
@@ -1,7 +1,71 @@
-package Model;
-
-/**
- * Created by Vahid on 5/5/2016.
- */
-public class EnemyGroup {
+public class EnemyGroup
+{
+	private ArrayList<Enemy> enemies;
+	private int xp;
+	private int money;
+	private int battleNum;
+	
+	public EnemyGroup(ArrayList<Enemy> enemies, int xp, int money, int battleNum)
+	{
+		setXP(xp);
+		setMoney(money);
+		setBattleNum(battleNum);
+		setEnemies(enemies);
+	}
+	
+	
+	public void setXP(int xp)
+	{
+		this.xp = xp;
+	}
+	
+	
+	public int getXP()
+	{
+		return xp;
+	}
+	
+	
+	public void setMoney(int money)
+	{
+		this.money = money;
+	}
+	
+	
+	public int getMoney()
+	{
+		return money;
+	}
+	
+	
+	public int setBattleNum(int battleNum)
+	{
+		this.battleNum = battleNum;
+	}
+	
+	
+	public int getBattleNum()
+	{
+		return battleNum;
+	}
+	
+	
+	public void setEnemies(ArrayList<Enemy> enemies)
+	{
+		this.enemies.addAll(enemies);
+	}
+	
+	
+	public Enemy getEnemy(String name, String ID)
+	{
+		for(Enemy enemy : enemies)
+		{
+			if(enemy.getName().equals(name) && enemy.getID().equals(ID))
+			{
+				return enemy;
+			}
+		}
+		
+		return null;
+	}
 }
--- TheGame/src/Model/Inventory.java
+++ TheGame/src/Model/Inventory.java
@@ -1,7 +1,55 @@
-package Model;
-
-/**
- * Created by Vahid on 5/5/2016.
- */
-public class Inventory {
+public class Inventory
+{
+	private int maxSize;
+	private int currentSize;
+	private ArrayList<Item> items;
+	
+	public Inventory(int size)
+	{
+		setMaxSize(size);
+	}
+	
+	
+	public void setMaxSize(int maxSize)
+	{
+		this.maxSize = maxSize;
+	}
+	
+	
+	public int getMaxSize()
+	{
+		return maxSize;
+	}
+	
+	
+	public void setSize(int size)
+	{
+		this.currentSize = size;
+	}
+	
+	
+	public int getCurrentSize()
+	{
+		return currentSize;
+	}
+	
+	
+	public void addItem(Item item)
+	{
+		items.add(item);
+	}
+	
+	
+	public void getItem(String itemName)
+	{
+		for(Item item : items)
+		{
+			if(item.getName().equals(itemName))
+			{
+				return item;
+			}
+		}
+		
+		return null;
+	}
 }
