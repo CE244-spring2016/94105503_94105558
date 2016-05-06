449c1251f368fc97d812efb8fb8116a836e7757f
Vahid <vbalazadehmeresht@ce.sharif.edu>
2016-05-06 23:13:39 +0430

    after a lot of badbakhti

--- TheGame/src/Model/EnemyVersion.java
+++ TheGame/src/Model/EnemyVersion.java
@@ -1,7 +1,40 @@
 package Model;
 
-/**
- * Created by Vahid on 5/5/2016.
- */
-public class EnemyVersion {
+import java.util.*;
+
+public class EnemyVersion
+{
+	String name;
+	HashMap<String, Integer> data;
+	
+	public EnemyVersion(String name, HashMap<String, Integer> data)
+	{
+		this.data = new HashMap<>();
+		setData(data);
+		setName(name);
+	}
+	
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	
+	public void setData(HashMap<String, Integer> data)
+	{
+		this.data.putAll(data);
+	}
+	
+	
+	public HashMap<String, Integer> getData()
+	{
+		return data;
+	}
 }
