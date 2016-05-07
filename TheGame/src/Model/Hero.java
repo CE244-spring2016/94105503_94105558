8f2051f17c50c6c48fc8dd476a624367ab96f280
ThePkms <r.sekaleshfar@gmail.com>
2016-05-08 03:22:30 +0430

    Hero constructor and variables updated

--- TheGame/src/Model/Hero.java
+++ TheGame/src/Model/Hero.java
@@ -10,18 +10,18 @@ public class Hero extends Warrior {
     private static int money;
     private static int XP;
     private static int immortalityPotionNum;
+	//Two more static variables probobly needed
 
     private ArrayList<Ability> abilities;
     private Inventory inventory;
     private HashMap<String, Integer> data;
 
-    public Hero(HashMap<String, Integer> data, int inventorySize, ArrayList<Ability> abilities) {
-        this.abilities = abilities;
-        this.data = data;
+    public Hero(HashMap<String, Integer> data, int inventorySize, ArrayList<Ability> abilities)
+	{
         this.inventory = new Inventory(inventorySize);
+        setAbilities(abilities);
+		setData(data);
     }
-
-    public HashMap<String, Integer> getData() {
-        return data;
-    }
+	
+	// Add setter getter please LOL
 }
