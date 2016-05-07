e6bdfd93b2b4f1d782876c07967568858e3f2f22
ThePkms <r.sekaleshfar@gmail.com>
2016-05-08 03:23:20 +0430

    Ability constructor and variables updated

--- TheGame/src/Model/Ability.java
+++ TheGame/src/Model/Ability.java
@@ -9,16 +9,33 @@ import java.util.concurrent.ForkJoinPool;
 /**
  * Created by Vahid on 5/5/2016.
  */
-public class Ability {
-    protected String name;
-    protected HashMap<String, ArrayList<Formula>> formulas;
-    protected ArrayList<Integer> upgradeXPs;
+public abstract class Ability 
+{
     protected int cooldownTurn;
     protected int currentUpgradeNum;
-    protected ArrayList<HashMap<String, Integer>> requiredAbilities;
-    protected Hero hero;
-    protected ArrayList<Integer> luckPercents;
+    protected String name;
     protected String abilityTarget;
-    protected boolean instantEffective;
-
+    protected Hero hero;
+    protected ArrayList<Integer> upgradeXPs;
+    protected ArrayList<Integer> luckPercents;
+	protected ArrayList<Integer> abilityCooldownNums;
+    protected ArrayList<HashMap<String, Integer>> requiredAbilities;
+    protected HashMap<String, ArrayList<Formula>> formulas;
+    protected boolean instantEffective; // Maybe we should handle this in a subclass??
+	
+	public Ability(String name, String abilityTarget, Hero hero, ArrayList<Integer> upgradeXPs, ArrayList<Integer> luckPercents, ArrayList<Integer> abilityCooldownNums, ArrayList<HashMap<String, Integer>> requiredAbilities, HashMap<String, ArrayList<Formula>> formulas)
+	{
+		cooldownTurn = 0;
+		currentUpgradeNum = 0;
+		setName(name);
+		setAbilityTarget(abilityTarget);
+		setHero(hero);
+		setUpgradeXPs(upgradeXPs);
+		setLuckPercents(luckPercents);
+		setAbilityCooldownNums(abilityCooldownNums);
+		setRequiredAbilities(requiredAbilities);
+		setFormulas(formulas);
+	}
+	
+	// Add setter getter please LOL
 }
