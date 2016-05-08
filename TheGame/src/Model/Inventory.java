b88a5f70bf914a1810f1cf68cc44be0526edf779
Vahid <vbalazadehmeresht@ce.sharif.edu>
2016-05-09 00:28:22 +0430

    inventoryChanges

--- TheGame/src/Model/Inventory.java
+++ TheGame/src/Model/Inventory.java
@@ -1,61 +1,56 @@
 package Model;
 
-import Model.Item;
-
 import java.util.ArrayList;
 
 public class Inventory
 {
-	private int maxSize;
-	private int currentSize;
-	private ArrayList<Item> items;
-	
-	public Inventory(int size)
-	{
-		setMaxSize(size);
-	}
-	
-	
-	public void setMaxSize(int maxSize)
-	{
-		this.maxSize = maxSize;
-	}
-	
-	
-	public int getMaxSize()
-	{
-		return maxSize;
-	}
-	
-	
-	public void setSize(int size)
-	{
-		this.currentSize = size;
-	}
-	
-	
-	public int getCurrentSize()
-	{
-		return currentSize;
-	}
-	
-	
-	public void addItem(Item item)
-	{
-		items.add(item);
-	}
-	
-	
-	public Item getItem(String itemName)
-	{
-		for(Item item : items)
-		{
-			if(item.getName().equals(itemName))
-			{
-				return item;
-			}
-		}
-		
-		return null;
-	}
+    private int maxSize;
+    private int currentSize;
+    private ArrayList<Item> items;
+
+    public Inventory(int size)
+    {
+        setMaxSize(size);
+    }
+
+    public int getMaxSize()
+    {
+        return maxSize;
+    }
+
+    public void setMaxSize(int maxSize)
+    {
+        this.maxSize = maxSize;
+    }
+
+    public void setSize(int size)
+    {
+        this.currentSize = size;
+    }
+
+
+    public int getCurrentSize()
+    {
+        return currentSize;
+    }
+
+
+    public void addItem(Item item)
+    {
+        items.add(item);
+    }
+
+
+    public Item getItem(String itemName)
+    {
+        for (Item item : items)
+        {
+            if (item.getName().equals(itemName))
+            {
+                return item;
+            }
+        }
+
+        return null;
+    }
 }
