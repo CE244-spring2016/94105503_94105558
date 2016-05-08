4d21066882a12809cb315d78c2e6399943a64dd8
ThePkms <r.sekaleshfar@gmail.com>
2016-05-08 19:32:00 +0430

    Enemy class updated

--- TheGame/src/Model/Enemy.java
+++ TheGame/src/Model/Enemy.java
@@ -6,20 +6,7 @@ import java.util.HashMap;
 /**
  * Created by Vahid on 5/5/2016.
  */
-public class Enemy extends Warrior {
-    private HashMap<String, Integer> data;
-    private InetSocketAddress name;
-    private InetSocketAddress ID;
-
-    public HashMap<String, Integer> getData() {
-        return data;
-    }
-
-    public InetSocketAddress getName() {
-        return name;
-    }
-
-    public InetSocketAddress getID() {
-        return ID;
-    }
+public class Enemy extends Warrior 
+{
+	
 }
