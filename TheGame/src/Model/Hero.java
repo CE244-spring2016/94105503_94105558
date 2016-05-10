71e9ec14cb0d2ca33ba18543ba9605dc5fdd036a
ThePkms <r.sekaleshfar@gmail.com>
2016-05-10 18:21:50 +0430

    Hero mostly done

--- TheGame/src/Model/Hero.java
+++ TheGame/src/Model/Hero.java
@@ -17,6 +17,8 @@ public class Hero extends Warrior
     private ArrayList<Ability> abilities = new ArrayList<>();
     private Inventory inventory;
     private HashMap<String, Integer> data = new HashMap<>();
+	private int criticalChance; // Check with teammate
+	private int nonTargetedEnemiesShare; // Check with teammate
 
     public Hero(String heroName, String heroClassName, HashMap<String, Integer> data, int inventorySize, ArrayList<Ability> abilities)
     {
@@ -27,73 +29,149 @@ public class Hero extends Warrior
         setData(data);
     }
 
+	
     public static int getMoney()
     {
         return money;
     }
 
+	
     public static void setMoney(int money)
     {
         Hero.money = money;
     }
 
+	
     public static int getXP()
     {
         return XP;
     }
 
+	
     public static void setXP(int XP)
     {
         Hero.XP = XP;
     }
 
+	
     public static int getImmortalityPotionNum()
     {
         return immortalityPotionNum;
     }
 
+	
     public static void setImmortalityPotionNum(int immortalityPotionNum)
     {
         Hero.immortalityPotionNum = immortalityPotionNum;
     }
 
+	
     public Inventory getInventory()
     {
         return inventory;
     }
 
+	
     public void setInventory(Inventory inventory)
     {
         this.inventory = inventory;
     }
 
+	
     public ArrayList<Ability> getAbilities()
     {
         return abilities;
     }
 
+	
     public void setAbilities(ArrayList<Ability> abilities)
     {
         this.abilities = abilities;
     }
 
+	
     public HashMap<String, Integer> getData()
     {
         return data;
     }
 
+	
     public void setData(HashMap<String, Integer> data)
     {
         this.data = data;
     }
 
+	
     public String getHeroClassName()
     {
         return heroClassName;
     }
 
+	
     public void setHeroClassName(String heroClassName)
     {
         this.heroClassName = heroClassName;
     }
+	
+	
+	public void useAbility(String abilityName, ArrayList<Enemy> enemies, ArrayList<Hero> allies)
+	{
+		Ability ability = findAbility(abilityName);
+		ArrayList<Warrior> abilityTargets = new ArrayList<>();
+		if(ability != null)
+		{
+			String target = ability.setAbilityTarget();
+			if(target.equals("himself"))
+			{
+				abilityTargets.add(this);
+			}
+			else if(target.equals("an ally"))
+			{
+				int targetIndex = Luck(0, allies.size() - 1);
+				abilityTargets.add(allies.get(targetIndex));
+			}
+			else if(target.equals("all allies"))
+			{
+				abilityTargets.addAll(allies);
+			}
+			else if(target.equals("an enemy"))
+			{
+				int targetIndex = Luck(0, enemies.size() - 1);
+				abilityTargets.add(enemies.get(targetIndex));
+			}
+			else if(target.equals("all enemies"))
+			{
+				abilityTargets.addAll(enemies);
+			}
+			else if(target.equals("everyone"))
+			{
+				abilityTargets.addAll(allies);
+				abilityTargets.addAll(enemies);
+			}
+			else
+			{
+				//invalid input
+			}
+			
+			ability.castAbility(abilityTargets);
+		}
+		else
+		{
+			//invalid input
+		}
+	}
+	
+	
+	public Ability findAbility(String abilityName)
+	{
+		for(Ability ability : abilities)
+		{
+			if(ability.getName().equals(abilityName))
+			{
+				return ability;
+			}
+		}
+		
+		return null;
+	}
 }
