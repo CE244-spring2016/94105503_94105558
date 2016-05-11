e01a445204694face23f7f3542bc345eb7353c75
Vahid <vbalazadehmeresht@ce.sharif.edu>
2016-05-12 00:57:52 +0430

    luck was very lucky

--- TheGame/src/Auxiliary/Luck.java
+++ TheGame/src/Auxiliary/Luck.java
@@ -1,8 +1,30 @@
 package Auxiliary;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+import java.util.Random;
+
 /**
  * Created by Vahid on 5/5/2016.
  */
 public class Luck
 {
+    public static boolean isLikely(int luckPercent)
+    {
+        if(luckPercent < 100)
+        {
+            Random rand = new Random();
+            int n = rand.nextInt(100) + 1;
+            if (n >= 1 && n <= luckPercent)
+                return true;
+            else
+                return false;
+        } else
+            return true;
+
+    }
+    public static int getRandom(int first, int second) {
+        Random random = new Random();
+        return (random.nextInt(second - first + 1) + first);
+    }
 }
